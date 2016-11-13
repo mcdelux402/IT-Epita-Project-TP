ce041d170b0095e7884f165c57f362a343c402fc
Victor Goutard <victor.goutard@epita.fr>
2016-11-13 23:38:51 +0100

    dernier tp

--- 20161107_list/vector/vector.c
+++ 20161107_list/vector/vector.c
@@ -1,27 +1,7 @@
-# ifndef VECTOR_H_
-# define VECTOR_H_
-
 # include <stdlib.h>
-
-
-struct vector {
-  size_t        capacity, size;
-  int          *data;
-}vect;
-
-// returns false if something goes wrong (realloc fails)
-int double_vector_size(struct vector *vect)
-{
-  int *tmp = realloc(vect->data, 2 * vect->capacity * sizeof (int));
-  if (tmp == NULL) {
-    // oups no more memory ?
-    warn("realloc of data fails");
-    return 0;
-  }
-  vect->data = tmp;
-  vect->capacity *= 2;
-  return 1;
-}
+# include <string.h>
+# include <stdio.h>
+# include "vector.h"
 
 /*
  * vector_make(capacity) create an empty vector
@@ -29,14 +9,27 @@ int double_vector_size(struct vector *vect)
  */
 struct vector* vector_make(size_t capacity)
 {
-  
+  struct vector *vect;
+  vect -> capacity = capacity;
+  vect -> size = 0;
+  vect -> data = NULL;
+  return vect;
 }
 
 /*
  * vector_push_back(vect, x) add x at the end of vect
  * if vect is full increase capacity
  */
-void vector_push_back(struct vector *vect, int x);
+void vector_push_back(struct vector *vect, int x)
+{
+  if (vect -> capacity == vect -> size)
+    vect -> capacity += 1;
+  vect -> size += 1;
+  size_t i = 0;
+  while(vect -> data[i] != NULL)
+    i++;
+  vect -> data[i] = x;
+}
 
 /*
  * vector_pop_back(vect, &x) get the last element of vect
@@ -45,13 +38,36 @@ void vector_push_back(struct vector *vect, int x);
  * return true (!=0) if size > 0
  * return false (0) otherwise
  */
-int  vector_pop_back(struct vector *vect, int *x);
+int  vector_pop_back(struct vector *vect, int *x)
+{
+  size_t i = 0;
+  while(vect -> data[i] != NULL)
+    i++;
+  *x = vect -> data[i];
+  vect -> data[i] = NULL;
+  vect -> size -= 1;
+  if (vect -> size < 0)
+    return 0;
+  return 1;
+}
 
 /*
  * vector_push_front(vect, x) add x at the beginning of vect
  * if vect is full increase capacity
  */
-void vector_push_front(struct vector *vect, int x);
+void vector_push_front(struct vector *vect, int x)
+{
+  size_t i = 0;
+  if (vect -> size == vect -> capacity)
+    vect -> capacity += 1;
+  vect -> size += 1;
+  while(vect -> data[i+1] != NULL)
+    {
+      i++;
+      vect -> data[i+1] = vect -> data[i];
+    }
+  vect -> data[0] = x;
+}
 
 /*
  * vector_pop_back(vect, &x) get the first element of vect
@@ -60,7 +76,19 @@ void vector_push_front(struct vector *vect, int x);
  * return true (!=0) if size > 0
  * return false (0) otherwise
  */
-int  vector_pop_front(struct vector *vect, int *x);
+int  vector_pop_front(struct vector *vect, int *x)
+{
+  size_t i = 0;
+  *x = vect -> data[i];
+  while(vect -> data[i+1] != NULL)
+    {
+      vect -> data[i] = vect -> data[i+1];
+      i++;
+    }
+  vect -> size -= 1;
+  vect -> data[i] = NULL;
+  return *x;
+}
 
 /*
  * vector_insert_at(vect, pos, x) add x in pos cell of vect
@@ -70,7 +98,35 @@ int  vector_pop_front(struct vector *vect, int *x);
  * vector_insert_at(v, 0, x) is equivalent to vector_push_front(v, x)
  * if vect is full increase capacity
  */
-int vector_insert_at(struct vector *vect, size_t pos, int x);
+int vector_insert_at(struct vector *vect, size_t pos, int x)
+{
+  if (pos == vect -> size)
+    {
+      vector_push_back(vect, x);
+      return 1;
+    }
+  else
+    {
+      if(pos == 0)
+	{
+	  vector_push_front(0, x);
+	  return 0;
+	}
+      else
+	{
+	  if(vect -> size == vect -> capacity)
+	    vect -> capacity += 1;
+	  vect -> size += 1;
+	  for(size_t i = pos; vect -> data[i+1] != NULL; i++)
+	    {
+	      vect -> data[i+1] = vect -> data[i]; 
+	    }
+	  vect -> data[pos] = x;
+	  return 1;
+	}
+    }  
+  
+}
 
 /*
  * vector_extract_at(vect, pos, &x) get the pos element of vect
@@ -87,3 +143,54 @@ int vector_extract_at(struct vector *vect, size_t pos, int *x);
  * both storage contains the same elements but are not at the same memory place
  */
 struct vector* vector_clone(struct vector *vect);
+
+
+void print_tab(struct vector *vect){
+  int line = 1;
+  printf("[");
+  size_t i = 0;
+  while(i < vect->size){
+    line += printf(";");
+    if(line > 72){
+      printf("\n");
+      line = 1;
+    }
+    line += printf(" %d", vect->data[i]);
+    i++;
+  }
+  printf(" ]\n");
+}
+
+int main() {
+  struct vector *vect = vector_make(5);
+  for(int i = 9; i >= 0; --i)
+    vector_push_front(vect, i);
+  print_tab(vect);
+  for(int i = 9; i >= 0; --i)
+    vector_push_back(vect, i);
+  print_tab(vect);
+  vector_insert_at(vect, 10, 42);
+  vector_insert_at(vect, 0, -1);
+  vector_insert_at(vect, vect->size, -666);
+  print_tab(vect);
+  int a;
+  vector_extract_at(vect, vect->size-1, &a);
+  int b;
+  vector_extract_at(vect, 0, &b);
+  int c;
+  vector_extract_at(vect, 10, &c);
+  printf("%d %d %d:", a, b, c);
+  print_tab(vect);
+  struct vector *clone = vector_clone(vect);
+  vector_push_back(vect, 1337);
+  int d;
+  vector_pop_front(clone, &d);
+  printf("%d:", d);
+  print_tab(clone);
+  print_tab(vect);
+  int x;
+  while(vector_pop_back(vect, &x))
+    printf("%d; ", x);
+  printf("\n");
+  print_tab(vect);
+}
